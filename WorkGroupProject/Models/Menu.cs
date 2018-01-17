7a9772d947bf2f2ae5b33079d9bfb3a8a45d7283
amisey <amirseydilo@gmail.com>
2018-01-17 15:51:03 +0330

    Add menu model

---
+++ WorkGroupProject/Models/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WorkGroupProject.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public ICollection<Menu> Children { get; set; } = new HashSet<Menu>();
+    }
+}
